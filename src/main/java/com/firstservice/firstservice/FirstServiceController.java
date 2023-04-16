30ef04d079c238f6c80da0781ce9c906d4a313ec
email <drodudals1@gmail.com>
2023-04-17 01:09:29 +0900

    first-service config

--- src/main/java/com/firstservice/firstservice/FirstServiceController.java
+++ src/main/java/com/firstservice/firstservice/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.firstservice.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +32,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First service.";
+    public String check(HttpServletRequest request) {
+        log.info("server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 }
